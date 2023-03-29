98038e26a3b49a5720df3de7304c557c4891a06a
Romashka <karmawayso@gmail.com>
2023-03-30 01:48:35 +0300

    added new func test_login_page()

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,27 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_login_page()
