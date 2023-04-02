843c3437ebd0598e45994752d9bbeef4f24aaa47
Romashka <karmawayso@gmail.com>
2023-04-02 03:56:15 +0300

    added new function

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 
 class BasePage:
@@ -22,4 +24,18 @@ class BasePage:
         get_url = self.browser.current_url
         sub_string_url = get_url.split('/')[-1]
         return sub_string_url == correct_substring, print(sub_string_url)
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
 
