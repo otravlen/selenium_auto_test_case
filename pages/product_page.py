c44b2ffdcbcb766f176b639e90caa699400a1382
Romashka <karmawayso@gmail.com>
2023-04-04 02:19:32 +0300

    added func should_not_be_success_message and success_message_is_disappeared

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,7 +8,6 @@ class ProductPage(BasePage):
         basket_link = self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET)
         basket_link.click()
 
-
     def text_after_add_to_basket(self):
         name_link = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         name = name_link.text
@@ -23,3 +22,10 @@ class ProductPage(BasePage):
         price_in_basket = price_link_in_basket.text
         print(price_in_basket)
         assert (name == name_in_basket) and (price == price_in_basket), "Name and price not found"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Element not disappeared"
