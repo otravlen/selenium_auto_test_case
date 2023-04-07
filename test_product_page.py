08a65b696ba6b5ea9906915c59de1cea630cdb21
Romashka <karmawayso@gmail.com>
2023-04-07 21:20:36 +0300

    added login tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+from .pages.basket_page import BasePage
 import pytest
 import time
 
@@ -28,6 +30,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.text_after_add_to_basket()
 
 
+
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
@@ -36,10 +39,12 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_not_be_success_message()
 
 
+
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
 
 
 @pytest.mark.xfail
@@ -50,11 +55,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.success_message_is_disappeared()
 
 
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
@@ -63,3 +70,36 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
     assert True, "guest can`t go to login page from product page"
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page = BasketPage(browser, browser.current_url)
+    page.is_the_basket_is_empty()
+
+
+@pytest.mark.login
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function")
+    def setup(self):
+        self.page = BasePage()
+        self.page.register_new_user()
+        self.page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.text_after_add_to_basket()
+
+    @pytest.mark.xfail
+    def test_user_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_not_be_success_message()
+
+    pass
