5ca5130913785011f394a846951d86f7db9170f5
Romashka <karmawayso@gmail.com>
2023-04-07 21:18:59 +0300

    added  BasketPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -8,6 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTER_FORM_EMAIL = (By.CSS_SELECTOR, '[name="registration-email"]')
+    REGISTER_FORM_PASSWORD = (By.CSS_SELECTOR, '[name="registration-password"]')
+    REGISTER_FORM_PASSWORD_2 = (By.CSS_SELECTOR, '[name="registration-password2"]')
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '[name="registration_submit"]')
+
 
 
 class ProductPageLocators:
@@ -15,10 +20,19 @@ class ProductPageLocators:
     PRODUCT_NAME = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
     PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
-    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    PRODUCT_PRICE_IN_BASKET = (
+    By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
 
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, '.icon-user')
+
+
+class BasketPageLocators:
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group > a.btn-default.btn")
+    EMPTY_BASKET_LINK = (By.CSS_SELECTOR, "#content_inner > p")
+    PRODUCTS_IN_BASKET = (By.CSS_SELECTOR, ".basket-title")
+    pass
