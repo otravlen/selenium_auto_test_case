1ecde95a69488cbc8f32f1836532775fff3a4e19
Romashka <karmawayso@gmail.com>
2023-04-07 21:20:06 +0300

    added func test_guest_cant_see_product_in_basket_opened_from_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -25,3 +26,11 @@ def test_login_page(browser):
     page.go_to_login_page()
     page = LoginPage(browser, browser.current_url)
     page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page = BasketPage(browser, browser.current_url)
+    page.is_the_basket_is_empty()
